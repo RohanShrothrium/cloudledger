3ff39f5af03811d47271fc5292e0c5aa1ce5d466
Deepak H R <deepakhr1999@gmail.com>
2019-05-03 22:56:48 +0530

    deployment setup init

---
+++ swarm/network/populate_hostname.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -ev
+
+ENV_LOCATION=$PWD/.env
+echo $ENV_LOCATION
+source $ENV_LOCATION
+
+FLAG="-i"
+ARCH=$(uname)
+if [ "$ARCH" == "Linux" ]; then
+  FLAG="-i"
+elif [ "$ARCH" == "Darwin" ]; then
+  FLAG="-it"
+fi
+
+# Zookeeper + Kafka
+sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG1_HOSTNAME/g" $ZK_COMPOSE_PATH
+sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG1_HOSTNAME/g" $KAFKA_COMPOSE_PATH
+
+# ORG1
+ORG1_CA_PATH=$(ls /var/mynetwork/certs/crypto-config/peerOrganizations/org1.example.com/ca/ | grep "_sk")
+sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG1_HOSTNAME/g" $ORDERER0_COMPOSE_PATH
+sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG1_HOSTNAME/g" $PEER_ORG1_COMPOSE_PATH
+sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG1_HOSTNAME/g" $SERVICE_ORG1_COMPOSE_PATH
+sed "$FLAG" "s#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/.*#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/$ORG1_CA_PATH#g" $SERVICE_ORG1_COMPOSE_PATH
+
+
+# ORG2
+ORG2_CA_PATH=$(ls /var/mynetwork/certs/crypto-config/peerOrganizations/org2.example.com/ca/ | grep "_sk")
+sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG2_HOSTNAME/g" $ORDERER1_COMPOSE_PATH
+sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG2_HOSTNAME/g" $PEER_ORG2_COMPOSE_PATH
+sed "$FLAG" "s/- node.hostname == .*/- node.hostname == $ORG2_HOSTNAME/g" $SERVICE_ORG2_COMPOSE_PATH
+sed "$FLAG" "s#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/.*#- FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/$ORG2_CA_PATH#g" $SERVICE_ORG2_COMPOSE_PATH
+
+
+if [ "$ARCH" == "Darwin" ]; then
+  rm */**.ymlt
+fi
---
+++ swarm/network/start_all.sh
@@ -0,0 +1,6 @@
+./scripts/network/deploy_services_kafka.sh
+./scripts/network/deploy_services_org1.sh
+./scripts/network/deploy_services_org2.sh
+
+
+
