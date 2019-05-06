27e635d422cd89540a6f0ee5badc76077732d032
rohanshrothrium <shrothriumrohan@gmail.com>
2019-05-06 14:01:41 +0530

    removed quotes

--- swarm/network/scripts/network/deploy_services_org3.sh
+++ swarm/network/scripts/network/deploy_services_org3.sh
@@ -5,7 +5,6 @@ source $GLOBAL_ENV_LOCATION
 set -ev
 
 # ORG 3
-docker stack deploy -c "$ORDERER2_COMPOSE_PATH" hlf_orderer
 sleep 3
 docker stack deploy -c "$SERVICE_ORG3_COMPOSE_PATH" hlf_services
 sleep 3
--- swarm/network/scripts/network/deploy_services_org4.sh
+++ swarm/network/scripts/network/deploy_services_org4.sh
@@ -5,7 +5,6 @@ source $GLOBAL_ENV_LOCATION
 set -ev
 
 # ORG 4
-docker stack deploy -c "$SERVICE_ORG4_COMPOSE_PATH" hlf_services
 sleep 3
 docker stack deploy -c "$PEER_ORG4_COMPOSE_PATH" hlf_peer
 
