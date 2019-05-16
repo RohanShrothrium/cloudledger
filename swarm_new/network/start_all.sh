c7635dbe374605e59b0b844045dc07de31f51cf0
rohanshrothrium <shrothriumrohan@gmail.com>
2019-05-16 23:55:10 +0530

    no TLS

--- swarm_new/network/start_all.sh
+++ swarm_new/network/start_all.sh
@@ -1,6 +1,6 @@
 for i in {1..5}; do
      export BYFN_CA"${i}"_PRIVATE_KEY=$(ls /var/mynetwork/certs/crypto-config/peerOrganizations/org${i}.example.com/ca/ | grep _sk)
-     echo BYFN_CA"${i}"_PRIVATE_KEY
+     echo $BYFN_CA"${i}"_PRIVATE_KEY
 done
 ./scripts/network/deploy_services_kafka.sh
 ./scripts/network/deploy_services_org1.sh
