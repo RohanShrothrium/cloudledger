d9128b9dd347bbb95d807b786e17f9bf83ac392e
rohanshrothrium <shrothriumrohan@gmail.com>
2019-05-17 00:10:16 +0530

    env

--- swarm_new/network/scripts/network/deploy_services_kafka.sh
+++ swarm_new/network/scripts/network/deploy_services_kafka.sh
@@ -3,6 +3,9 @@
 GLOBAL_ENV_LOCATION=$PWD/.env
 source $GLOBAL_ENV_LOCATION
 set -ev 
+for i in {1..5}; do
+     export BYFN_CA"${i}"_PRIVATE_KEY=$(ls /var/mynetwork/certs/crypto-config/peerOrganizations/org${i}.example.com/ca/ | grep _sk)
+done
 
 # KAFKA & ZOOKEEPER
 docker stack deploy -c "$ZK_COMPOSE_PATH" hlf_zk 
