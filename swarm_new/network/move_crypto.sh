9d36177664680049ab12a1dc3e4de08dc0586455
deepakhr1999 <deepakhr1999@gmail.com>
2019-05-31 16:42:22 +0530

    add org scripts updated

--- swarm_new/network/move_crypto.sh
+++ swarm_new/network/move_crypto.sh
@@ -5,16 +5,18 @@ rm -rf mkdir /var/mynetwork/*
 
 mkdir -p /var/mynetwork/chaincode
 mkdir -p /var/mynetwork/certs
+mkdir -p /var/mynetwork/certs/scripts
 mkdir -p /var/mynetwork/bin
 mkdir -p /var/mynetwork/fabric-src
 
-git clone https://github.com/hyperledger/fabric /var/mynetwork/fabric-src/hyperledger/fabric
-cd /var/mynetwork/fabric-src/hyperledger/fabric
-# git checkout release-1.4
-cd -
+# git clone https://github.com/hyperledger/fabric /var/mynetwork/fabric-src/hyperledger/fabric
+# cd /var/mynetwork/fabric-src/hyperledger/fabric
+# # git checkout release-1.4
+# cd -
 cp -R crypto-config /var/mynetwork/certs/
 cp -R config /var/mynetwork/certs/
 cp -R ../chaincodes/* /var/mynetwork/chaincode/
 cp -R bin/* /var/mynetwork/bin/
-
+cp -R scripts/add_org/* /var/mynetwork/certs/scripts/	
+echo "If prompted, please enter password to chown /var/mynetwork"
 sudo chown -R $USER /var/mynetwork
