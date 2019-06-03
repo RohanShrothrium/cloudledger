59af638a68b5d72bdc945f67df9cb741dbfd34c8
rohanshrothrium <shrothriumrohan@gmail.com>
2019-06-03 15:16:51 +0530

    start all

--- swarm_new/network/start_all.sh
+++ swarm_new/network/start_all.sh
@@ -1,12 +1,6 @@
-for i in {1..5}; do
-     export BYFN_CA"${i}"_PRIVATE_KEY=$(ls /var/mynetwork/certs/crypto-config/peerOrganizations/org${i}.example.com/ca/ | grep _sk)
-done
 ./scripts/network/deploy_services_kafka.sh
 ./scripts/network/deploy_services_org1.sh
 ./scripts/network/deploy_services_org2.sh
 ./scripts/network/deploy_services_org3.sh
 ./scripts/network/deploy_services_org4.sh
 ./scripts/network/deploy_services_org5.sh
-
-
-
