11d3ca38d93346461295a5a77e08160aa864af50
rohanshrothrium <shrothriumrohan@gmail.com>
2020-02-27 00:32:33 +0530

    networkdown vuln removed

--- cloud-network/networkdown.sh
+++ cloud-network/networkdown.sh
@@ -4,7 +4,3 @@ docker rm $(docker ps -aq)
 rm -r crypto-config
 rm -r channel-artifacts
 docker rmi $(docker image ls | awk '/mycc/{print $3}')
-cd /home/rohan/Documents/cloudledger/SDK/wallet
-rm -rf *
-cd ~
-rm -rf .hfc-key-store
